be334b4969f7d49fdef0cf796161231781834350
codetheprogram <85871294+codetheprogram@users.noreply.github.com>
2023-07-11 15:25:44 -0700

    Add files via upload

---
+++ shop-script.js
@@ -0,0 +1,14 @@
+function toggleClass() {
+     const taska = document.getElementById("taska");
+     const taskb = document.getElementById("taskb");
+     const taskc = document.getElementById("taskc");
+     const taskd = document.getElementById("taskd");
+     const taske = document.getElementById("taske");
+     const taskbar = document.getElementById("task-bar");
+     taska.classList.toggle("task-A");
+     taskb.classList.toggle("task-A");
+     taskc.classList.toggle("task-A");
+     taskd.classList.toggle("task-A");
+     taske.classList.toggle("task-A");
+     taskbar.classList.toggle("task-bar-A");
+};
